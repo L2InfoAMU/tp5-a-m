c4727fd7d15c1da95980bf6a0a1e2dd7761d7436
git <yashthevalil@gmail.com>
2018-11-28 23:12:23 +0100

    Finished Task 4

--- src/main/java/viewer/Display.java
+++ src/main/java/viewer/Display.java
@@ -6,7 +6,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.PixelWriter;
-import javafx.scene.paint.Color;
 
 import java.net.URL;
 import java.util.ResourceBundle;
