641ed1aaa14819bebe06312d831e4fba2c763660
git <yashthevalil@gmail.com>
2018-11-28 19:57:43 +0100

    Added checks to second Constructor

--- src/main/java/image/BruteRasterImage.java
+++ src/main/java/image/BruteRasterImage.java
@@ -1,20 +1,27 @@
 package image;
 
+import util.Matrices;
+
 import java.awt.*;
 
 public class BruteRasterImage {
     private Color[][] colors;
 
     public BruteRasterImage(Color color, int width, int height) {
-        this.colors = new Color[height][width];
-        for (int i = 0; i < height; i++) {
-            for (int j = 0; j < width; j++) {
+        this.colors = new Color[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
                 this.colors[i][j] = color;
             }
         }
     }
 
     public BruteRasterImage(Color[][] colors) {
+        Matrices.requiresNonZeroDimensions(colors);
+        Matrices.requiresNonNull(colors);
+        Matrices.requiresRectangularMatrix(colors);
         this.colors = colors;
     }
+
+
 }
