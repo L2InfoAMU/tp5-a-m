b7332311b90c741aa5322a8fdbd8ee9e4924bb3d
git <yashthevalil@gmail.com>
2018-11-28 22:26:24 +0100

    Created SparseRasterImage Class

---
+++ src/main/java/image/SparseRasterImage.java
@@ -0,0 +1,10 @@
+package image;
+
+import javafx.scene.paint.Color;
+
+public class SparseRasterImage extends RasterImage {
+    @Override
+    public Color getPixelColor(int x, int y) {
+        return null;
+    }
+}
