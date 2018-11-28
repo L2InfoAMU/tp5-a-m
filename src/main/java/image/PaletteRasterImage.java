7055f1cc90e09c3332fe0388f89fd795899a9cee
git <yashthevalil@gmail.com>
2018-11-28 21:13:31 +0100

    Fixed Constructors in PaletteRasterImage

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -3,28 +3,69 @@ package image;
 import javafx.scene.paint.Color;
 import util.Matrices;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class PaletteRasterImage implements Image {
 
-    private Color[][] colors;
+    private List<Color> palette = new ArrayList<>();
+    private byte[][] indexOfColors;
     private int width;
     private int height;
 
     public PaletteRasterImage(Color color, int width, int height) {
         this.width = width;
         this.height = height;
-        this.colors = new Color[width][height];
+        this.indexOfColors = new byte[width][height];
+        palette.add(color);
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                colors[i][j] = color;
+                addColorToPalette(color);
+                indexOfColors[i][j] = (byte) getColorIndexFromPalette(color);
             }
         }
     }
 
-    public PaletteRasterImage(Color[][] colors) {
-        Matrices.requiresNonZeroDimensions(colors);
-        Matrices.requiresNonNull(colors);
-        Matrices.requiresRectangularMatrix(colors);
-        this.colors = colors;
+    public PaletteRasterImage(Color[][] pixels) {
+        Matrices.requiresNonZeroDimensions(pixels);
+        Matrices.requiresNonNull(pixels);
+        Matrices.requiresRectangularMatrix(pixels);
+        this.width = pixels.length;
+        this.height = pixels[0].length;
+        this.indexOfColors = new byte[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                addColorToPalette(pixels[i][j]);
+                indexOfColors[i][j] = (byte) getColorIndexFromPalette(pixels[i][j]);
+            }
+        }
+    }
+
+    private boolean isColorInPalette(Color color) {
+        for (Color aPalette : palette) {
+            if (color.equals(aPalette)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private int getColorIndexFromPalette(Color color) {
+        for (int i = 0; i < palette.size(); i++) {
+            if (color.equals(palette.get(i))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void addColorToPalette(Color color) {
+        if (!isColorInPalette(color)) {
+            if (palette.size() == 128) {
+                throw new NotSupportedException("Palette is full");
+            }
+            palette.add(color);
+        }
     }
 
     @Override
